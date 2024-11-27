db1ac0bda74ea8ae037e9dbc77b92250ef1c62aa
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2024-11-27 11:01:02 +0000

    Fix ci build

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,5 +5,5 @@ pushd dis-authorisation-client-js
   make build
 
   # copy build to the location expected by the CI
-  cp -r build package.json package-lock.json Dockerfile.concourse ../build
+  cp -r build package.json package-lock.json ../build
 popd
