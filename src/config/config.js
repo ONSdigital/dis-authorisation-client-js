b8f67acc7534840e16e96efc971f00f38e1f8190
Rhys St Romaine <rhys.st.romaine@ons.gov.uk>
2024-12-12 16:24:18 +0000

    Refactor config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,7 @@
 // src/config/config.js
-const defaultConfig = {
+export const defaultConfig = {
   timeOffsets: {
-    passiveRenewal: 2000,
-    invasiveRenewal: 3000,
+    passiveRenewal: 300000,
   },
   onRenewSuccess: () => console.log('[LIBRARY] Session renewed successfully'),
   onRenewFailure: () => console.warn('[LIBRARY] Session renewal failed'),
@@ -12,9 +11,7 @@ const defaultConfig = {
   checkSessionOnInit: false,
 };
 
-const apiConfig = {
-  RENEW_SESSION: 'api/tokens/self',
-  CHECK_SESSION: 'api/tokens/self',
+export const apiConfig = {
+  RENEW_SESSION: '/tokens/self',
+  CHECK_SESSION: '/tokens/self',
 };
-
-export default { defaultConfig, apiConfig };
